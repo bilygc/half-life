070e091ab4588cd116cb90a838089f5c21dd4d6b
Bily <licbgomez@gmail.com>
2026-01-15 12:43:46 -0700

    change to use ipv4 connection as unique connection type

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,9 @@
 import postgres from 'postgres'
 
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
 
 
-const connectionString = env === 'development' ? process.env.DATABASE_URL_IPV4 : process.env.DATABASE_URL_IPV6
+const connectionString = process.env.DATABASE_URL_IPV4
 const sql = postgres(connectionString || "")
 
 export default sql
